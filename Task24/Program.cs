9070650704b870e9bb07ced7e8bda48cb6676861
AlexandrSawrasov <caiiiuk@mail.ru>
2022-11-07 22:58:40 +0300

    add Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+void GetSumNums(int number)
+{
+    int sum = (1 + number) / 2 * number;
+    Console.WriteLine($"Cумма цифр от 1 до введенного числа: {sum}");
+    return;
+}
+
+Console.Clear();
+
+GetSumNums(5);
