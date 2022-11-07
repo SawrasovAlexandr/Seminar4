10ccf0ddf1f30bf725668d69b8fc3e254c6437cb
AlexandrSawrasov <caiiiuk@mail.ru>
2022-11-07 23:18:33 +0300

    add Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+void GetProdNums(int number)
+{
+    int prod = 1;
+    for (int i = 1; i <= Math.Abs(number); i++) prod *= i;
+    Console.WriteLine($"Произведение цифр от 1 до введенного числа: {prod}");
+}
+
+Console.Clear();
+
+GetProdNums(5);
