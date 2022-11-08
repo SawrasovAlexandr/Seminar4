1ba2f07f4c0a5f12e4dd3848128b2090fedf5cc6
AlexandrSawrasov <caiiiuk@mail.ru>
2022-11-08 22:28:29 +0300

    add Task29 1

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,14 +1,30 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-int[] GetArr(int lenght)
+// int[] GetArr(int lenght)
+// {
+//     int[] result = new int[lenght];
+//     for (int i = 0; i < lenght; i++)
+//     {
+//         result[i] = new Random().Next(100);
+//     }
+//     return result;
+// }
+
+// int[] array = GetArr(8);
+// Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] InputArr(string input)
 {
-    int[] result = new int[lenght];
-    for (int i = 0; i < lenght; i++)
+    string[] str = input.Split();
+    int length = str.Length;
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
     {
-        result[i] = new Random().Next(100);
+       result[i] = int.Parse(str[i]);
     }
     return result;
 }
 
-int[] array = GetArr(8);
+Console.WriteLine("Введите числа через пробел: ");
+int[] array = InputArr(Console.ReadLine());
 Console.WriteLine($"[{String.Join(", ", array)}]");
