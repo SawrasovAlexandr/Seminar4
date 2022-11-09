2d3f4ac2d162b5ca64cc172f7a7f74911e268a3b
AlexandrSawrasov <caiiiuk@mail.ru>
2022-11-10 00:58:06 +0300

    add Task29 2

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -13,18 +13,44 @@
 // int[] array = GetArr(8);
 // Console.WriteLine($"[{String.Join(", ", array)}]");
 
-int[] InputArr(string input)
-{
-    string[] str = input.Split();
-    int length = str.Length;
-    int[] result = new int[length];
-    for (int i = 0; i < length; i++)
-    {
-       result[i] = int.Parse(str[i]);
-    }
-    return result;
-}
+// int[] InputArr(string input)
+// {
+//     string[] str = input.Split();
+//     int length = str.Length;
+//     int[] result = new int[length];
+//     for (int i = 0; i < length; i++)
+//     {
+//        result[i] = int.Parse(str[i]);
+//     }
+//     return result;
+// }
 
 Console.WriteLine("Введите числа через пробел: ");
 int[] array = InputArr(Console.ReadLine());
 Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] InputArr(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ' ') count++;
+    }
+    int[] result = new int[count];
+    int j = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ' ')
+        {
+            j++;
+           
+        }
+        else if (result[j] == 0) 
+        {
+            result[j] = int.Parse(input[i].ToString());
+        }
+        else result[j] = result[j] * 10 + int.Parse(input[i].ToString());
+    }
+    return result;
+}
+
